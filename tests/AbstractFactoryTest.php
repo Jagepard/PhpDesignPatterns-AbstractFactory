8d8da36aa2441a3ad72409395d11397b82750dba
Jagepard <dankorot@gmail.com>
2019-12-12 17:40:32 +0300

    Update test

--- tests/AbstractFactoryTest.php
+++ tests/AbstractFactoryTest.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 /**
- * @author    : Korotkov Danila <dankorot@gmail.com>
- * @license   https://mit-license.org/ MIT
+ * @author  : Jagepard <jagepard@yandex.ru>
+ * @license https://mit-license.org/ MIT
  */
 
 namespace Creational\AbstractFactory\Tests;
@@ -18,37 +18,22 @@ class AbstractFactoryTest extends PHPUnit_Framework_TestCase
     /**
      * @var string
      */
-    private $content;
-
-    protected function setUp(): void
-    {
-        $this->content = 'Message';
-    }
+    private $content = 'Message';
 
     public function testJson()
     {
-        $content     = \json_encode($this->content);
-        $jsonFactory = new JsonFactory();
+        $content = \json_encode(['content' => $this->content]);
 
-        $this->assertEquals($jsonFactory->createMessage($this->getContent())->getContent(), $content);
+        $this->assertEquals((new JsonFactory())->createMessage($this->content), $content);
     }
 
     public function testXML()
     {
-        $dom     = new \DOMDocument("1.0", "utf-8");
-        $content = $dom->createElement("content", $this->getContent());
+        $dom = new \DOMDocument("1.0", "utf-8");
+        $content = $dom->createElement("content", $this->content);
         $dom->appendChild($content);
         $xmlFactory = new XMLFactory();
 
-
-        $this->assertEquals($xmlFactory->createMessage($this->getContent())->getContent(), $dom->saveXML());
-    }
-
-    /**
-     * @return string
-     */
-    public function getContent(): string
-    {
-        return $this->content;
+        $this->assertEquals($xmlFactory->createMessage($this->content), $dom->saveXML());
     }
 }
